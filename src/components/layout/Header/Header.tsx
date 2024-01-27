293f408231cddf0efde5ed4eac22dfe5ef3b15da
evgen <evgst.gl@gmail.com>
2024-01-27 21:04:20 +0700

    fix

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,11 +1,10 @@
 import s from './Header.module.scss';
 import {FC} from 'react';
-import {Logo} from '../../../data/data';
 const Header: FC = () => {
     return (
         <div className={s.pageHeader}>
             <a href={'https://github.com/evscoder/react-starter'} className={s.pageHeader__title} target={'_blank'}>
-                <img src={Logo} alt={'React Starter'} width={140} /> React Starter
+                React Starter
             </a>
         </div>
     );
