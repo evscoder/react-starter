4e28bf52437715861f05ac9375f740c15da1cca1
evgen <evgst.gl@gmail.com>
2024-02-10 18:47:13 +0700

    fix img url

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,12 +1,12 @@
 import s from './Header.module.scss';
 import {FC} from 'react';
 import Icon from '../../ui/Icon/Icon';
-import imageURL from '../../../helper/imagePath';
+import imagePath from "../../../helper/imagePath";
 const Header: FC = () => {
     return (
         <div className={s.pageHeader}>
             <a href={'https://github.com/evscoder/react-starter'} className={s.pageHeader__title} target={'_blank'}>
-                <img src={imageURL('Logo.svg')} alt={'React Starter'} width={120}></img>
+                <img src={imagePath('Logo.svg')} alt={'React Starter'} width={120}></img>
                 <Icon name={'icon-keyboard-down'} />
                 React Starter
             </a>
--- src/helper/imagePath.ts
+++ src/helper/imagePath.ts
@@ -1,7 +1,7 @@
-const imageURL = (pathName: string) => {
+const imagePath = (pathName: string) => {
     const fullPath = process.env.NODE_ENV === 'production' ? `/react-starter/img/content/` : `/img/content/`;
 
     return fullPath + pathName;
 };
 
-export default imageURL;
+export default imagePath;
